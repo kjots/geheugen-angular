249f2578cdc43684e95d559812423ce34817a931
Karl Juri Ots <kjots@kjots.org>
2016-08-05 17:27:09 +1000

    Updated to create memos on demand.

--- src/index.js
+++ src/index.js
@@ -8,28 +8,4 @@ export default angular.module('geheugen', [])
     .constant('Memo', Memo)
     .constant('memos.config', memos.config)
     .value('memos.registry', memos.registry)
-    .provider('memos', () => memos.provider)
-    .run(['$q', '$injector', ($q, $injector) => {
-        Object.keys(memos.config).forEach(ensureMemo);
-
-        function ensureMemo(name) {
-            if (memos.registry[name] === undefined) {
-                let { opts, factory } = memos.config[name];
-                let dependencies = opts.dependencies || [];
-
-                memos.registry[name] = new Memo({
-                    Q: $q,
-                    singleton: opts.singleton !== undefined ? opts.singleton : true,
-                    dependencies: dependencies.map(ensureMemo),
-                    onReset: opts.onReset !== undefined ? () => $injector.invoke(opts.onReset) : undefined,
-                    factory: values => $injector.invoke(factory, undefined, dependencies.reduce((locals, dependency, i) => {
-                        locals[dependency] = values[i];
-
-                        return locals;
-                    }, {}))
-                });
-            }
-
-            return memos.registry[name];
-        }
-    }]);
+    .provider('memos', () => memos.provider);
--- src/memos.js
+++ src/memos.js
@@ -1,30 +1,59 @@
+import { Memo } from 'geheugen';
+
 export const config = {};
 export const registry = {};
 
 export function provider(name, ...args) {
     if (args.length === 0) {
-        return () => registry[name].resolve();
+        return ['$q', '$injector', ($q, $injector) => ensureMemo($q, $injector, name).resolve()];
     }
 
     let [ opts, factory ] = args.length === 1 ? [ {}, ...args ] : args;
 
     config[name] = { opts, factory };
 
-    return () => {
-        let memo = () => registry[name].resolve();
+    return ['$q', '$injector', ($q, $injector) => {
+        let $memo = ensureMemo($q, $injector, name);
 
-        memo.reset = () => registry[name].reset();
-        memo.resetDependants = () => registry[name].resetDependants();
+        let memo = () => $memo.resolve();
+
+        memo.reset = () => $memo.reset();
+        memo.resetDependants = () => $memo.resetDependants();
 
         return memo;
-    };
+    }];
 }
 
-provider.$get = () => {
-    let memos = name => registry[name].resolve();
+provider.$get = ['$q', '$injector', ($q, $injector) => {
+    let memos = name => ensureMemo($q, $injector, name).resolve();
 
-    memos.reset = name => registry[name].reset();
-    memos.resetDependants = name => registry[name].resetDependants();
+    memos.reset = name => ensureMemo($q, $injector, name).reset();
+    memos.resetDependants = name => ensureMemo($q, $injector, name).resetDependants();
 
     return memos;
-};
+}];
+
+function ensureMemo($q, $injector, name) {
+    if (registry[name] === undefined) {
+        registry[name] = createMemo($q, $injector, name);
+    }
+
+    return registry[name];
+}
+
+function createMemo($q, $injector, name) {
+    let { opts, factory } = config[name];
+    let dependencies = opts.dependencies || [];
+
+    return new Memo({
+        Q: $q,
+        singleton: opts.singleton !== undefined ? opts.singleton : true,
+        dependencies: dependencies.map(dependency => ensureMemo($q, $injector, dependency)),
+        onReset: opts.onReset !== undefined ? () => $injector.invoke(opts.onReset) : undefined,
+        factory: values => $injector.invoke(factory, undefined, dependencies.reduce((locals, dependency, i) => {
+            locals[dependency] = values[i];
+
+            return locals;
+        }, {}))
+    });
+}
