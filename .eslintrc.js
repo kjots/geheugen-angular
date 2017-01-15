b1a48fca2c7647937acd55209b7882d3eeda25b3
Karl Juri Ots <kjots@kjots.org>
2017-01-16 00:41:01 +1100

    Updated ESLint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,19 @@ module.exports = {
         sourceType: 'module'
     },
     rules: {
-        'strict': [ 'error', 'global' ],
-        'indent': [ 'error', 4 ],
+        'brace-style': [ 2, 'stroustrup', { allowSingleLine: true } ],
+        'comma-dangle': [ 'error', 'never' ],
+        'eqeqeq': [ 'error', 'always' ],
+        'indent': [ 'error', 4, { SwitchCase: 1 } ],
+        'keyword-spacing': [ 'error', { before: true, after: true } ],
         'linebreak-style': [ 'error', 'unix' ],
+        'no-cond-assign': [ 'error', 'always' ],
+        'no-unneeded-ternary': [ 'error', { defaultAssignment: false } ],
+        'no-var': 'error',
         'quotes': [ 'error', 'single' ],
-        'semi': [ 'error', 'always' ]
+        'semi': [ 'error', 'always' ],
+        'space-before-blocks': [ 'error', 'always' ],
+        'strict': [ 'error', 'global' ],
+        'yoda': [ 'error', 'never' ]
     }
 };
